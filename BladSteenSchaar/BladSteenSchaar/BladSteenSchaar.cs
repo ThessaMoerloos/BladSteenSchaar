6d9760b60c90fc15867e41771475fd6394d6a654
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2015-10-21 14:30:23 +0200

    Structuur toegevoegd

---
+++ BladSteenSchaar/BladSteenSchaar/BladSteenSchaar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BladSteenSchaar
+{
+    public partial class BladSteenSchaar : Form
+    {
+
+
+        public BladSteenSchaar()
+        {
+
+            InitializeComponent();
+
+        }
+
+
+
+        private void BladSteenSchaar_Load(object sender, EventArgs e)
+        {
+
+            /*YahtzeeController yController = new YahtzeeController();
+
+            YahtzeeView yahtzeeView = yController.getView();
+
+            Controls.Add(yahtzeeView);*/
+
+        }
+    }
+}
